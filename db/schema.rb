c292087c92a4262488be134f42b74aa5e6db80f1
Elukoye <lukoyedith@gmail.com>
2019-11-14 15:11:47 +0000

    added password colums to User table in db

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191114115910) do
+ActiveRecord::Schema.define(version: 20191114150923) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
+    t.string "password_confirmation"
+    t.string "password"
   end
 
 end
