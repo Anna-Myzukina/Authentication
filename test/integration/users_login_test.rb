80ee49f54c52add28cef0e896b50336ff0241409
Elukoye <lukoyedith@gmail.com>
2019-11-15 16:50:10 +0000

    added login page and login layout test

---
+++ test/integration/users_login_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class UsersLoginTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:michael)
+  end
+  
+   test "login with valid information" do
+    get login_path
+    post login_path, params: { session: { email:    @user.email,
+                                          password: 'password' } }
+    assert_redirected_to @user
+    follow_redirect!
+   
+    assert_select "a[href=?]", login_path, count: 0
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", user_path(@user)
+  end
+end
