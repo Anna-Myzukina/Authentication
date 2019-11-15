6a01f1466c1347cdcc9015e287795ff967e2518e
Anna Myzukina <anna.muzykina83@gmail.com>
2019-11-15 17:55:52 +0200

    Add signup user and signup tests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,9 +15,10 @@ class UsersController < ApplicationController
   end
 
   def create
+    flash.now[:success] = "Welcome to The Clubhouse App!"
    @user = User.new(user_params)
       if @user.save
-        redirect_to new_user_path
+        redirect_to @user
       else
         render :new
       end
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,10 @@ Rails.application.routes.draw do
 
   resources :users
   root 'users#index'
-  get    '/signup',  to: 'users#new'
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
+  get    '/signup',  to: 'users#new'
+  post '/signup',  to: 'users#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
