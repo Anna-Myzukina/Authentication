7ea76bba9c6aeec1595a204b3a68d4011a28de36
Elukoye <lukoyedith@gmail.com>
2019-11-15 13:58:19 +0000

    add method to create user in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,10 @@
 class UsersController < ApplicationController
   def index
-    @users = User.all
+    @user = User.all
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def new
@@ -14,7 +15,7 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params)
+   @user = User.new(user_params)
       if @user.save
         redirect_to new_user_path
       else
@@ -27,4 +28,11 @@ class UsersController < ApplicationController
 
   def destroy
   end
+  
+   private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
 end
