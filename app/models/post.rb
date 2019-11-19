607b530011fc5d06ae049d84ee5a299fbd4b58fc
Elukoye <lukoyedith@gmail.com>
2019-11-19 11:27:52 +0000

    added tests for Posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
+  validates :user_id, presence: true
+  
+  validates :body, presence: true, length: { maximum: 140 }
 end
--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,28 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:michael)
+    # This code is not idiomatically correct.
+    @post = Post.new(body: "Lorem ipsum", user_id: @user.id)
+  end
+
+  test "should be valid" do
+    assert @post.valid?
+  end
+
+  test "user id should be present" do
+    @post.user_id = nil
+    assert_not @post.valid?
+  end
+  
+  test "content should be present" do
+    @post.content = "   "
+    assert_not @post.valid?
+  end
+
+  test "content should be at most 140 characters" do
+    @post.content = "a" * 141
+    assert_not @post.valid?
+  end
 end
