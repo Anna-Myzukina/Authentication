fc3c7cb548309f3e96514e65a2c1f7462ccf5d5d
Elukoye <lukoyedith@gmail.com>
2019-11-21 10:08:42 +0000

    modified routes

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
-  resources :microposts,          only: [:create, :destroy]
+  resources :microposts,          only: [:new, :create, :index] 
  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
